1584c5ac49e7b3fa5ade54092f486893dac7dc35
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-07-19 22:49:09 -0400

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+    BRANDS << brand unless BRANDS.any? do
+      # push into brands UNLESS current brand already == one that has been
+      #paassed in
+      |b| b == brand
+    end
+
   end
 
   def cobble
